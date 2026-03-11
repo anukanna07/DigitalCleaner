a86ede9630361e743301aea8a9a07c5c1c8dabf5
anukanna07 <anukanna73@gmail.com>
2026-03-11 20:18:07 +0530

    Scanned

--- src/main/java/servlet/ScanServlet.java
+++ src/main/java/servlet/ScanServlet.java
@@ -1,48 +1,110 @@
 package servlet;
 
-import java.io.IOException;
-import java.util.Collection;
+import java.io.*;
+import java.util.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.*;
 
-import javax.servlet.http.Part;
+import utils.ImageAnalyzer;
+import utils.FileInfo;
 
 @WebServlet("/ScanServlet")
-@MultipartConfig
+
+@MultipartConfig(
+fileSizeThreshold=1024*1024*2,
+maxFileSize=1024*1024*50,
+maxRequestSize=1024*1024*100
+)
+
 public class ScanServlet extends HttpServlet {
 
-    protected void doPost(HttpServletRequest request,
-                          HttpServletResponse response)
-            throws ServletException, IOException {
+protected void doPost(
+HttpServletRequest request,
+HttpServletResponse response)
 
-        // Session check
-        HttpSession session = request.getSession(false);
+throws ServletException, IOException {
 
-        if(session == null || session.getAttribute("loggedIn") == null) {
-            response.sendRedirect("login.html");
-            return;
-        }
 
-        // Get uploaded files
-        Collection<Part> files = request.getParts();
+HttpSession session =
+request.getSession(false);
 
-        int count = 0;
+if(session==null ||
+session.getAttribute("loggedIn")==null){
 
-        for(Part file : files){
+response.sendRedirect("login.html");
 
-            if(file.getSubmittedFileName()!=null &&
-               file.getSubmittedFileName().length()>0){
+return;
 
-                count++;
-            }
-        }
+}
 
-        request.setAttribute("totalScreenshots", count);
+/* GET UPLOADED FILES */
 
-        request.getRequestDispatcher("result.jsp")
-               .forward(request,response);
-    }
+Collection<Part> parts =
+request.getParts();
+
+List<File> files =
+new ArrayList<>();
+
+/* temp upload folder */
+
+String uploadPath =
+getServletContext()
+.getRealPath("")
++File.separator+
+"tempUploads";
+
+File uploadDir =
+new File(uploadPath);
+
+if(!uploadDir.exists()){
+
+uploadDir.mkdir();
+
+}
+
+/* save uploaded images */
+
+for(Part part : parts){
+
+String fileName =
+part.getSubmittedFileName();
+
+if(fileName!=null &&
+fileName.endsWith(".png") ||
+fileName.endsWith(".jpg") ||
+fileName.endsWith(".jpeg")){
+
+File file =
+new File(uploadPath+
+File.separator+
+fileName);
+
+part.write(
+file.getAbsolutePath()
+);
+
+files.add(file);
+
+}
+
+}
+
+/* analyze images */
+
+List<FileInfo> analyzedFiles =
+ImageAnalyzer.analyze(files);
+
+request.setAttribute(
+"files",
+analyzedFiles
+);
+
+request
+.getRequestDispatcher("result.jsp")
+.forward(request,response);
+
+}
 }
